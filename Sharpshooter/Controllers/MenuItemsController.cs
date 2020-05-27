625386392b82231ddaacbe7886099ce70a74a220
Joel Jagarath <joey.jagarath05@gmail.com>
2020-05-27 14:29:51 +0200

    My First Commit

---
+++ Sharpshooter/Controllers/MenuItemsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Sharpshooter.Models;
+using System.IO;
+
+namespace Sharpshooter.Controllers
+{
+    public class MenuItemsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: MenuItems
+        public ActionResult Index()
+        {
+            var menuItems = db.MenuItems.Include(m => m.Menu).Include(m => m.MenuGroup);
+            return View(menuItems.ToList());
+        }
+
+        public ActionResult ViewMenuItems()
+        {
+            var menuItems = db.MenuItems.Include(m => m.Menu).Include(m => m.MenuGroup);
+            return View(menuItems.ToList());
+        }
+
+        // GET: MenuItems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuItem menuItem = db.MenuItems.Find(id);
+            if (menuItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menuItem);
+        }
+
+        // GET: MenuItems/Create
+        public ActionResult Create()
+        {
+            ViewBag.MenuID = new SelectList(db.Menus, "MenuID", "MenuTitle");
+            ViewBag.MenuGroupID = new SelectList(db.MenuGroups, "MenuGroupID", "MenuGroupTitle");
+            return View();
+        }
+
+        // POST: MenuItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MenuItemID,MenuID,MenuGroupID,MenuItemTitle,MenuItemDescription,MenuItemNutrition,MenuItemIngredients,MenuItemQuantity,MenuItemImg,ImageFile,MenuItemCost")] MenuItem menuItem/*, HttpPostedFileBase MenuItemImg*/)
+        {
+            if (ModelState.IsValid)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(menuItem.ImageFile.FileName);
+                string extension = Path.GetExtension(menuItem.ImageFile.FileName);
+                fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                menuItem.MenuItemImg = "~/Content/ItemImages/" + fileName;
+                fileName = Path.Combine(Server.MapPath("~/Content/ItemImages/"), fileName);
+                menuItem.ImageFile.SaveAs(fileName);
+
+
+
+                //MenuItem m = new MenuItem();
+                //m.MenuItemTitle = menuItem.MenuItemTitle;
+                //m.MenuItemImg = MenuItemImg.FileName.ToString();
+
+                //var path = Server.MapPath("~/Content/ItemImages/");
+                //MenuItemImg.SaveAs(Path.Combine(path, MenuItemImg.FileName.ToString()));
+
+                db.MenuItems.Add(menuItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MenuID = new SelectList(db.Menus, "MenuID", "MenuTitle", menuItem.MenuID);
+            ViewBag.MenuGroupID = new SelectList(db.MenuGroups, "MenuGroupID", "MenuGroupTitle", menuItem.MenuGroupID);
+            return View(menuItem);
+        }
+
+        // GET: MenuItems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuItem menuItem = db.MenuItems.Find(id);
+            if (menuItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MenuID = new SelectList(db.Menus, "MenuID", "MenuTitle", menuItem.MenuID);
+            ViewBag.MenuGroupID = new SelectList(db.MenuGroups, "MenuGroupID", "MenuGroupTitle", menuItem.MenuGroupID);
+            return View(menuItem);
+        }
+
+        // POST: MenuItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MenuItemID,MenuID,MenuGroupID,MenuItemTitle,MenuItemDescription,MenuItemNutrition,MenuItemIngredients,MenuItemQuantity,MenuItemImg,ImageFile,MenuItemCost")] MenuItem menuItem)
+        {
+            if (ModelState.IsValid)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(menuItem.ImageFile.FileName);
+                string extension = Path.GetExtension(menuItem.ImageFile.FileName);
+                fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                menuItem.MenuItemImg = "~/Content/ItemImages/" + fileName;
+                fileName = Path.Combine(Server.MapPath("~/Content/ItemImages/"), fileName);
+                menuItem.ImageFile.SaveAs(fileName);
+
+
+                db.Entry(menuItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MenuID = new SelectList(db.Menus, "MenuID", "MenuTitle", menuItem.MenuID);
+            ViewBag.MenuGroupID = new SelectList(db.MenuGroups, "MenuGroupID", "MenuGroupTitle", menuItem.MenuGroupID);
+            return View(menuItem);
+        }
+
+        // GET: MenuItems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuItem menuItem = db.MenuItems.Find(id);
+            if (menuItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menuItem);
+        }
+
+        // POST: MenuItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MenuItem menuItem = db.MenuItems.Find(id);
+            db.MenuItems.Remove(menuItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Sharpshooter/Controllers/MenusController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Sharpshooter.Models;
+
+namespace Sharpshooter.Controllers
+{
+    public class MenusController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Menus
+        public ActionResult Index()
+        {
+            return View(db.Menus.ToList());
+        }
+
+        public ActionResult ViewMenuAll()
+        {
+            var categories = db.Menus.ToList();
+            return View(categories);
+        }
+
+
+        public ActionResult Browse(string category)
+        {
+            var categoryModel = db.Menus.Include("MenuItems").Single(c => c.MenuTitle == category);
+            return View(categoryModel);
+            
+        }
+        // GET: Menus/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Menu menu = db.Menus.Find(id);
+            if (menu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menu);
+        }
+
+        public ActionResult ViewItems(int id)
+        {
+            MenuItem menuItem = new MenuItem();
+            menuItem = db.MenuItems.Where(x => x.MenuItemID == id).FirstOrDefault();
+            
+            return View(menuItem);
+        }
+
+        public ActionResult ViewMenu(int id)
+        {
+            var Item = db.MenuItems.Find(id);
+            return View(Item);
+        }
+
+        // GET: Menus/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Menus/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MenuID,MenuTitle,MenuDescription")] Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Menus.Add(menu);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(menu);
+        }
+
+        // GET: Menus/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Menu menu = db.Menus.Find(id);
+            if (menu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menu);
+        }
+
+        // POST: Menus/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MenuID,MenuTitle,MenuDescription")] Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(menu).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(menu);
+        }
+
+        // GET: Menus/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Menu menu = db.Menus.Find(id);
+            if (menu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menu);
+        }
+
+        // POST: Menus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Menu menu = db.Menus.Find(id);
+            db.Menus.Remove(menu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
