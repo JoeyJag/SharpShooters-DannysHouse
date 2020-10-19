7b77de532b7303ddcb1cebe27bca0b4012043be2
CryptoDroid <42205589+Ratul0307@users.noreply.github.com>
2020-10-19 10:40:50 +0200

    Added graphs for Daily sales & Sales per item

--- Sharpshooter/Controllers/HomeController.cs
+++ Sharpshooter/Controllers/HomeController.cs
@@ -3,21 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using Sharpshooter.Models;
 
 namespace Sharpshooter.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult DateSales()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
+        }
+
+        public ActionResult SalesChart()
+        {
+            return View();
+        }
+
+        public ActionResult GetDataDate()
+        {
+
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var query = context.OrderDetails.Include("Orders")
+                .GroupBy(p => p.Order.OrderDate)
+                .Select(g => new { name = g.Key, count = g.Sum(w => w.OrderId) }).ToList();
+            return Json(query, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetData()
+        {
+
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var query = context.OrderDetails.Include("MenuItem")
+                .GroupBy(p => p.MenuItem.MenuItemTitle)
+                .Select(g => new { name = g.Key, count = g.Sum(w => w.Quantity) }).ToList();
+            return Json(query, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult Contact()
